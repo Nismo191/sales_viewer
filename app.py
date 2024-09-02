173b979931b1e432a5cd2c7a7af81353f103cf93
Nismo191 <nismo191@googlemail.com>
2024-09-02 15:52:48 +0100

    Added dataframe cleanup

--- app.py
+++ app.py
@@ -22,24 +22,26 @@ response = requests.get(url)
 # Raise an error if the request was unsuccessful
 response.raise_for_status()
 
-
 # Convert the CSV data to a pandas DataFrame
 df = pd.read_csv(StringIO(response.text), sep=",", index_col=False)
+df.drop(df.tail(1).index,inplace=True)
+df = df[df['Item number'].notna()]
 
-# Display the DataFrame
-# print(df.head())
+
+df = df.rename(columns=lambda x: x.strip())
+
 
 df.replace({r'[^\x00-\x7F\u00A3]+':''}, regex=True, inplace=True)
 
-df = df.drop(columns=['Item number', 'Payout AD', 'Payout NS', 'Payout JP', 'Payout MP', 'Due to NS', 'Due to JP', 'Due to MP'])
+df = df.drop(columns=['Item number', 'Payout AD', 'Payout NS', 'Payout JP', 'Payout MP', 'Due to NS', 'Due to JP', 'Due to MP'], axis=1)
 
 st.title("Dentons of Dudley")
 
 
 password = st.text_input("Password", type="password")
 
-df = df[df['Password'] ==  password]
+df = df[df['Password'] == password]
 
-df = df.drop(columns=['Email', 'Password'])
+df = df.drop(columns=['Password'])
 
-st.dataframe(df.head(), height=750, hide_index=True)
+st.dataframe(df, height=750, hide_index=True)
